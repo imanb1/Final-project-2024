18c3057cc1160355715728614e94538aa98a183b
Iman Bowman <112996041+imanb1@users.noreply.github.com>
2024-04-04 19:08:49 -0400

    Add files via upload
    Final project proposal

---
+++ final_proposal.py
@@ -0,0 +1,16 @@
+# Title: Crossword Puzzle generator
+
+# We are going to create a program that generates different words hidden in an array
+
+# Our project will create a Python program that makes crossword puzzles.  We will implement user input words and clues and the program will arrange them into a gride.
+# It will allow users to adjust the grid's size and difficulty level.  It will use python libraries for textr processing and creating a user interface, ensuring a simple and customizable solution for making crossword puzzles.
+
+# I am collaborating with Suubi Nakaddu. Her email address is Tup80280@temple.edu and her TA is Xinwen.
+
+# A good outcome will be that the program runs efficiently and correctly when the user inputs the a particular word.
+# I will accomplish completing a final project whether it runs or not
+
+# I believe we can finish a fully functioning crossword puzzle by the deadline.  If we happen to not fully complete it I believe we would still have msjority of it done
+
+# The steps we have to take to complete our project is to brainstorm and assign each person with sections to complete.  We will both help each other with creating an outline and then collaborately complete each section of the project.
+# we will utilize the help of our TAs and online if we need any help with certain parts of the lab.
